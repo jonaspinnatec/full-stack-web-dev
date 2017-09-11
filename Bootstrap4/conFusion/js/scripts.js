4ca5ac390ca3953ddada57f56d03c9e1b66a5f94
Jonas Oeschger <jonas.oeschger@pinnatec.ch>
2017-09-11 12:13:25 +0200

    bug fix

--- Bootstrap4/conFusion/js/scripts.js
+++ Bootstrap4/conFusion/js/scripts.js
@@ -1,16 +1,15 @@
-<script>
-    $(document).ready(function(){
-      $("#carousel-button").click(function(){
-          if ($("#carousel-button").children("span").hasClass('fa-pause')) {
-              $("#mycarousel").carousel('pause');
-              $("#carousel-button").children("span").removeClass('fa-pause');
-              $("#carousel-button").children("span").addClass('fa-play');
-          }
-          else if ($("#carousel-button").children("span").hasClass('fa-play')){
-              $("#mycarousel").carousel('cycle');
-              $("#carousel-button").children("span").removeClass('fa-play');
-              $("#carousel-button").children("span").addClass('fa-pause');
-          }
-      });
-    });
-</script>
+
+$(document).ready(function(){
+  $("#carousel-button").click(function(){
+      if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+          $("#mycarousel").carousel('pause');
+          $("#carousel-button").children("span").removeClass('fa-pause');
+          $("#carousel-button").children("span").addClass('fa-play');
+      }
+      else if ($("#carousel-button").children("span").hasClass('fa-play')){
+          $("#mycarousel").carousel('cycle');
+          $("#carousel-button").children("span").removeClass('fa-play');
+          $("#carousel-button").children("span").addClass('fa-pause');
+      }
+  });
+});
