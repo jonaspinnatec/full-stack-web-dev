3899ea0fc1eeb5ec6f65735e974bb584f2901e62
Jonas Oeschger <jonas.oeschger@pinnatec.ch>
2017-10-17 10:25:31 +0200

    Directives

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { baseURL } from './shared/baseurl';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 import { RestangularModule, Restangular } from 'ngx-restangular';
 import { RestangularConfigFactory } from './shared/restConfig';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { RestangularConfigFactory } from './shared/restConfig';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
