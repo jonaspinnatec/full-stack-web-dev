96422738632ee770054667f5809e3e89eb96384b
Jonas Oeschger <jonas.oeschger@pinnatec.ch>
2017-11-07 08:47:31 +0100

    Alerts, Toasts and Loading - Part 2

--- Ionic/conFusion/src/pages/favorites/favorites.ts
+++ Ionic/conFusion/src/pages/favorites/favorites.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ItemSliding, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ItemSliding, ToastController, LoadingController, AlertController } from 'ionic-angular';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { Dish } from '../../shared/dish';
 
-/**
- * Generated class for the FavoritesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-favorites',
@@ -22,8 +16,9 @@ export class FavoritesPage implements OnInit {
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private favoriteservice: FavoriteProvider,
     @Inject('BaseURL') private BaseURL,
-    public toastCtrl: ToastController) {
-  }
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController) {}
 
   ngOnInit() {
     this.favoriteservice.getFavorites()
@@ -37,14 +32,37 @@ export class FavoritesPage implements OnInit {
 
   deleteFavorite(item: ItemSliding, id: number) {
     console.log('delete', id);
-    this.favoriteservice.deleteFavorite(id)
-      .subscribe(favorites => this.favorites = favorites,
-        errmess => this.errMess = errmess);
-    this.toastCtrl.create({
-      message: 'Dish ' + id + ' deleted successfully',
-      duration: 3000}).present();
+
+    let alert = this.alertCtrl.create({
+      title: 'Confirm Delete',
+      message: 'Do you want to delete Dish '+ id,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Delete cancelled');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            let loading = this.loadingCtrl.create({
+              content: 'Deleting . . .'
+            });
+            let toast = this.toastCtrl.create({
+              message: 'Dish ' + id + ' deleted successfully',
+              duration: 3000});
+            loading.present();
+            this.favoriteservice.deleteFavorite(id)
+              .subscribe(favorites => {this.favorites = favorites; loading.dismiss(); toast.present(); } ,
+                errmess =>{ this.errMess = errmess; loading.dismiss(); });
+          }
+        }
+      ]
+    });
+
+    alert.present();
     item.close();
   }
-
-
 }
