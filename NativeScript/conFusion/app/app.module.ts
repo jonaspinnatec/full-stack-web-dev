1d861e3edd75205c6afee08b71bf951946b8a88f
Jonas Oeschger <jonas.oeschger@pinnatec.ch>
2017-12-18 11:55:46 +0100

    Buttons

--- NativeScript/conFusion/app/app.module.ts
+++ NativeScript/conFusion/app/app.module.ts
@@ -16,6 +16,7 @@ import { AboutComponent } from './about/about.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { FavoriteService } from './services/favorite.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
@@ -53,7 +54,8 @@ import { baseURL } from './shared/baseurl';
         DishService,
         ProcessHTTPMsgService,
         PromotionService,
-        LeaderService
+        LeaderService,
+        FavoriteService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
