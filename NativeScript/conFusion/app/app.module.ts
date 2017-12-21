21dcf7f88a76442b927a600bae66fb9dc4dd9e6f
Jonas Oeschger <jonas.oeschger@pinnatec.ch>
2017-12-21 12:40:29 +0100

    Platform and Connectivity

--- NativeScript/conFusion/app/app.module.ts
+++ NativeScript/conFusion/app/app.module.ts
@@ -26,6 +26,7 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { FavoriteService } from './services/favorite.service';
 import { CouchbaseService } from './services/couchbase.service';
+import { PlatformService } from './services/platform.service';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { baseURL } from './shared/baseurl';
@@ -77,7 +78,8 @@ import { baseURL } from './shared/baseurl';
         PromotionService,
         LeaderService,
         FavoriteService,
-        CouchbaseService
+        CouchbaseService,
+        PlatformService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
