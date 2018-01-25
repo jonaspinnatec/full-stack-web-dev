adc584236b04fef238eab6cec528b3b2dcde928c
Jonas Oeschger <jonas.oeschger@pinnatec.ch>
2018-01-25 10:27:05 +0100

    Integrating Client and Server

--- NodeJS/conFusionServer/routes/favoritesRouter.js
+++ NodeJS/conFusionServer/routes/favoritesRouter.js
@@ -77,9 +77,28 @@ favoritesRouter.route('/')
 
 favoritesRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-.get(cors.cors, (req,res,next) => {
-    res.statusCode = 403;
-    res.end('GET operation not supported on /favorites/'+ req.params.dishId);
+.get(cors.cors, authenticate.verifyUser, (req,res,next) => {
+    Favorites.findOne({user: req.user._id})
+    .then((favorites) => {
+        if (!favorites) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({"exists": false, "favorites": favorites});
+        }
+        else {
+            if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": false, "favorites": favorites});
+            }
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": true, "favorites": favorites});
+            }
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     console.log('Adding Favorite: ' + req.params.dishId + ' for User: ' + req.user._id);
@@ -128,6 +147,18 @@ favoritesRouter.route('/:dishId')
         var index = favorites.dishes.indexOf(dishId);
         favorites.dishes.splice(dishId, 1);
         favorites.save()
+        .then((favorite) => {
+            Favorites.findById(favorite._id)
+            .populate('user')
+            .populate('dishes')
+            .then((favorite) => {
+                console.log('Favorite Dish Deleted!', favorite);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(favorite);
+            })
+        })
+
         .then((savedFavorites) => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
